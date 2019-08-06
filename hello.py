0e1772bea07689190e56ed224f68deb8dfa50fc1
John Roelofse <wjroelofse@gmail.com>
2019-08-06 21:00:22 +0100

    Start of flask web app

---
+++ hello.py
@@ -0,0 +1,11 @@
+from flask import Flask
+from flask import render_template
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return 'Index Page'
+
+@app.route('/hello')
+def hello(name=None):
+    return render_template('hello.html', name=name)

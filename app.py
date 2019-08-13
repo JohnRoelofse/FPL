c03d9a6aac9254db7e6071b9065dc93ee3d7177c
John Roelofse <wjroelofse@gmail.com>
2019-08-13 21:03:26 +0100

    Revert back to original

--- app.py
+++ app.py
@@ -5,8 +5,7 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     captains = gw_captains()
-    return 'I LOVE YOU NATALIE'
-    #return render_template('welcome_page.html', captains = captains)
+    return render_template('welcome_page.html', captains = captains)
 
 #if __name__ = "__main__":
 #    app.run()

d5b08c9fbbaf4c108b336e14bf66f65c196c0e8b
John Roelofse <wjroelofse@gmail.com>
2019-08-23 15:57:07 +0100

    Changed function name

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
 from flask import Flask, render_template
-from gameweek_captains import gw_captains
+from gameweek_functions import gw_captains
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    captains = gw_captains()
+    captains = gameweek_captains()
     return render_template('welcome_page.html', captains = captains)
 
 #if __name__ = "__main__":

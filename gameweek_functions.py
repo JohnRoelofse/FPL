191f65663c1078bcf9017fa537d2d2d7895eb98e
John Roelofse <wjroelofse@gmail.com>
2019-08-23 15:54:55 +0100

    Added check_gameweek function

--- gameweek_functions.py
+++ gameweek_functions.py
@@ -1,4 +1,51 @@
-def gw_captains():
+def check_gameweek():
+    """
+    This function checks what the current gameweek is and returns that value as an integer.
+
+    It does this by:
+        - Extracting a list of the gameweeks and their start times from the events table
+        - Converting the start time to a datetime.datetime object
+        - Comparing the gameweek start times to the time now
+
+    Notes:
+        - The gameweek start time is stored in the events table as a string but is in UTC format
+    """
+
+    import sqlite3
+    from datetime import datetime
+
+    # Make a connection to the fpl database
+    conn = sqlite3.connect('fpl.sqlite')
+    cur = conn.cursor()
+
+    # Extract list of gameweeks and the gameweek start time 
+    gameweek_time_start_list = cur.execute("select gameweek_id, deadline_time from events").fetchall()
+
+    # Close connection
+    conn.close()
+
+    # Create dictionary
+    gameweek_time_start_dict = dict()
+    for row in gameweek_time_start_list:
+        gameweek_time_start_dict[row[0]] = datetime.strptime(row[1], '%Y-%m-%dT%H:%M:%SZ')
+
+    # Check what gameweek it is and return the value as an integer
+    utcnow = datetime.utcnow()
+    for gameweek in gameweek_time_start_dict:
+        if gameweek == 1:
+            if utcnow < gameweek_time_start_dict[gameweek]:
+                return gameweek
+                break
+            elif utcnow > gameweek_time_start_dict[gameweek] and utcnow < gameweek_time_start_dict[gameweek+1]:
+                return gameweek
+                break
+        elif utcnow > gameweek_time_start_dict[gameweek] and utcnow < gameweek_time_start_dict[gameweek+1]:
+            return gameweek
+            break
+        elif gameweek == 38:
+            return 38
+
+def gameweek_captains():
     import sqlite3
 
     # Open connection to database

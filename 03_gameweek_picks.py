39d28a67ba18ae5fbfa373faf60e614051755b83
John Roelofse <wjroelofse@gmail.com>
2019-08-28 20:02:49 +0100

    Added check_gameweek() function

--- 03_gameweek_picks.py
+++ 03_gameweek_picks.py
@@ -1,5 +1,6 @@
 # Load required modules
 from authentication import authenticate
+from gameweek_functions import check_gameweek
 import requests
 import sqlite3
 
@@ -14,12 +15,18 @@ cur = conn.cursor()
 # Get list of fpl players from league table
 fpl_players = cur.execute('select entry_id from league').fetchall()
 
+# Get gameweek
+gameweek = check_gameweek()
+
 # Loop through all fpl players to get their picks for the week
 gameweek_picks = dict()
 for player in fpl_players:
-    get_request = session.get(f'https://fantasy.premierleague.com/api/entry/{player}/event/1/picks/')
-    js = get_request.json()
-    gameweek_picks[player] = js['picks']
+    try:
+        get_request = session.get(f'https://fantasy.premierleague.com/api/entry/{player}/event/{gameweek}/picks/')
+        js = get_request.json()
+        gameweek_picks[player] = js['picks']
+    except:
+        continue
     
 # Insert gameweek picks into gameweek_picks table
 for fpl_player in gameweek_picks:
@@ -31,7 +38,7 @@ for fpl_player in gameweek_picks:
                                                     is_captain,
                                                     is_vice_captain
                                                     ) VALUES (?, ?, ?, ?, ?, ?) """,
-                    (1, fpl_player, item['element'], item['position'], item['is_captain'], item['is_vice_captain']))
+                    (gameweek, fpl_player, item['element'], item['position'], item['is_captain'], item['is_vice_captain']))
 
 # Commit changes
 conn.commit()

c6764a7db7365d41e8f219c1a7e2fba853629eb6
John Roelofse <wjroelofse@gmail.com>
2019-09-02 20:49:59 +0100

    Add APScheduler python file

---
+++ clock.py
@@ -0,0 +1,9 @@
+from apscheduler.schedulers.blocking import BlockingScheduler
+
+sched = BlockingScheduler()
+
+@sched.scheduled_job('interval', minutes=3)
+def timed_job():
+    print('Bingo! It\'s working!')
+
+sched.start()

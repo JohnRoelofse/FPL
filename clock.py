757974a3e01bfa06d84dc7dc8643a246c6a305b1
John Roelofse <wjroelofse@gmail.com>
2019-09-02 21:19:34 +0100

    Changed to BackgroundScheduler

--- clock.py
+++ clock.py
@@ -1,9 +1,24 @@
-from apscheduler.schedulers.blocking import BlockingScheduler
+from pytz import utc
+from apscheduler.schedulers.background import BackgroundScheduler
+from apscheduler.jobstores.sqlalchemy import SQLAlchemyJobStore
+from apscheduler.executors.pool import ThreadPoolExecutor, ProcessPoolExecutor
+import sqlalchemy
 
-sched = BlockingScheduler()
+jobstores = {
+    'default': SQLAlchemyJobStore(url='sqlite:///jobs.sqlite', engine=sqlalchemy.create_engine('sqlite:///jobs.sqlite'))
+}
+executors = {
+    'default': ThreadPoolExecutor(20),
+    'processpool': ProcessPoolExecutor(5)
+}
+job_defaults = {
+    'coalesce': False,
+    'max_instances': 3
+}
+scheduler = BackgroundScheduler(jobstores=jobstores, executors=executors, job_defaults=job_defaults, timezone=utc)
 
-@sched.scheduled_job('interval', minutes=3)
-def timed_job():
-    print('Bingo! It\'s working!')
+def bingo():
+    print('Bingo! It\'s working!)
 
-sched.start()
+scheduler.add_job(bingo, 'interval', minutes=1)
+scheduler.start()
